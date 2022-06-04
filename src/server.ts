cef0c60ab12260ccf3c1a628db81f51c47f70c35
Rafael Felipe da Silva <48489919+RafaelFelps@users.noreply.github.com>
2022-06-04 16:51:44 -0300

    Add dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(3000, () => {
-  console.log(`Hello ${process.env.PROJECT_NAME}`);
+  console.log(`Server is running at http://localhost:3000/`);
 });
